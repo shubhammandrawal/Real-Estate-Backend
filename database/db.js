be80a3a87442fc152d4211a1c15c8cac3b8a2be9
shubham mandrawal <shubhammandrawal2826@gmail.com>
2023-08-10 11:14:54 +0530

    Backend-Structure

---
+++ database/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const dotenv = require("dotenv").config();
+
+const connectDatabase = () => {
+    mongoose.connect(process.env.MONGO_URL,
+        { family: 4 })
+        .then(() => {
+            console.log("Succesfully connected to database")
+        }).catch(() => {
+            console.log("Failed to connect to DB")
+        })
+}
+
+module.exports = connectDatabase;
+
