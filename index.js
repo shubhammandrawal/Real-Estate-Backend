6a9be6520d8dfc4909a88e1bdd2b38a5aea30b7f
shubham mandrawal <shubhammandrawal2826@gmail.com>
2023-08-13 18:40:26 +0530

    added post API for adding property

--- index.js
+++ index.js
@@ -9,8 +9,14 @@ connectDatabase();
 app.use(express.json())
 
 const basicInfo = require("./Routes/Addproperty")
+const propertDetails = require("./Routes/Addproperty")
+const generalInfo = require("./Routes/Addproperty")
+const locationInfo = require("./Routes/Addproperty")
 
 app.use("/api/v1", basicInfo)
+app.use("/api/v1", propertDetails)
+app.use("/api/v1", generalInfo)
+app.use("/api/v1", locationInfo)
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server is running on ${process.env.PORT}`)
